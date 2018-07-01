d6aebcc5f0242eebd519be817c50778c370e103c
Ramzi Youssef <contact@mediacept.com>
2018-07-01 17:15:35 +0100

    add vuetify

--- app/main/main.js
+++ app/main/main.js
@@ -1,12 +1,18 @@
 import axios from 'axios'
 import Vue from 'vue'
-
+import Vuetify from 'vuetify'
+ 
+Vue.use(Vuetify)
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'typeface-roboto'
 
 new Vue({
   el: '#app',
